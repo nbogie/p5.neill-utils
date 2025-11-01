b40355290287bcfeae03a3e04688682cdb5083b1
nbogie <neillbogie@googlemail.com>
2025-11-01 23:55:45 +0000

    initial incomplete. from yarn create vite with vanilla and ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function zip(arr1: any[], arr2: any[]): any[] {
+    console.log("doing stuff");
+    return arr1.concat(arr2);
+}
+
+export { zip };
---
+++ testManual/testScript.js
@@ -0,0 +1,2 @@
+console.log("testScript.js started");
+console.log(NeillP5Utils.zip([1, 2], "a b c".split(" ")));
